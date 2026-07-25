8bbca144b0d2f51bcfb6733dc667624655a19636
Ranjith Reddy KV <ranjithreddykv27@gmail.com>
2026-07-25 11:04:28 +0530

    file-explorer ui implementation

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -1,18 +1,53 @@
-"use client"
-import { usePlayground } from '@/modules/playground/hooks/usePlayground';
-import { useParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { usePlayground } from "@/modules/playground/hooks/usePlayground";
+import { useParams } from "next/navigation";
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+import { TemplateFileTree } from "@/modules/playground/components/playground.explorer";
 
 const MainPlaygroundPage = () => {
-    const {id} = useParams<{id:string}>();
-    const {playgroundData , templateData , isLoading , error ,loadPlayground, saveTemplateData} = usePlayground(id);
-    console.log("Template data : " ,templateData);
-    console.log("Playground data : " ,playgroundData);
-    return (
-    <div>
-        Params : {id}
-    </div>
-  )
-}
+  const { id } = useParams<{ id: string }>();
+  const {
+    playgroundData,
+    templateData,
+    isLoading,
+    error,
+    loadPlayground,
+    saveTemplateData,
+  } = usePlayground(id);
+  
+  const activeFile = "sample.txt"
+  return (
+    <TooltipProvider>
+      <>
+        <TemplateFileTree
+          data = {templateData!}
+          onFileSelect={()=>{}}
+          selectedFile={activeFile}
+          title = "File Explorer"
+          onAddFile = {()=>{}}
+          onAddFolder = {()=>{}}
+          onDeleteFile = {()=>{}}
+          onDeleteFolder = {()=>{}}
+          onRenameFile = {()=>{}}
+          onRenemeFolder={()=>{}}
+        />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+          </header>
+          <div className="flex flex-1 items-center gap-2">
+              <h1 className="text-sm font-medium">
+                {playgroundData?.title || "Code playground" }
+              </h1>
+          </div>
+        </SidebarInset>
+      </>
+    </TooltipProvider>
+  );
+};
 
-export default MainPlaygroundPage
+export default MainPlaygroundPage;
